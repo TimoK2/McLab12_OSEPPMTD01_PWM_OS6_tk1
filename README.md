1255d3ebc9549cc75ee5fb308ce1371eef7229ba
timo_k2 <null>
2021-05-03 08:09:26 +0000

    Initial commit. Works OK.

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-PWM example for students to work with a MOSFET circuit. 
+PWM example for students to work with a Full Bridge driver. 
 
-### License and contributions
+Using the PwmOut API and the OSEPP MTD-01 full bridge driver.
+ 
+Hardware
+L432KC or similar with PWM capable outputs.
+LED and 220 Ohm resistor.
+For testing full bridge driver: OSEPP MTD-01 + DC motor.
+The motor maximum 46 V and 2 A continuous 3 A 100 microseconds in starting.
+ 
+Connect:
+L432KC D3 -- 220 Ohm -- LED+ 
+L432KC GND -- LED-
 
-The software is provided under Apache-2.0 license. Contributions to this project are accepted under the same license. Please see contributing.md for more info.
+The MTD-01 digital inputs recognize 3,3 V as HIGH although logic operating voltage is 5 V. 
+ENA is the enable signal and is used for pwm speed control.
+L432KC D3 -- OSEPPMTD01 ENA
+L432KC D9 -- OSEPPMTD01 In1
+L432KC D10 -- OSEPPMTD01 In2
+L432KC 5V -- OSEPPMTD01 5V
+L432KC GND -- OSEPPMTD01 GND
+ 
+Disconnect the jumper JP1 on the MTD-01 !!!!!
+12V power -- OSEPPMTD01 VCC
+0V power -- OSEPPMTD01 GND
 
-This project contains code from other projects. The original license text is included in those source files. They must comply with our license guide.
+MOTOR + -- OSEPPMTD01 OUT1
+MOTOR - -- OSEPPMTD01 OUT2
+
+There are a number of other full bridge DC motor control boards available. 
+Please check the digital input functions before trying with other than the OSEPP MTD-01. 
+
+The software is provided under Apache-2.0 license. 
--- main.cpp
+++ main.cpp
@@ -4,22 +4,36 @@
  *********************************************
  *
  * PWM example using the PwmOut API
+ * and the OSEPP MTD-01 full bridge driver
  *
  *********************************************
- *Description: McLab12_MOSFET_PWM_OS6_tk1
+ *Description: McLab12_OSEPPMTD01_PWM_OS6_tk1
  * Hardware
  * L432KC or similar with PWM capable outputs.
  * The L432KC pin D13 for onboard LED is not PWM capable!
  * LED and 220 Ohm resistor
- * For testing MOSFET: IRL540 + protection diode + DC motor
+ * For testing full bridge driver: OSEPP MTD-01 + DC motor
+ * The motor maximum 46 V and 2 A continuous 3 A 100 microseconds in starting.
+ 
  * Connect:
- * L432KC D10 -- 220 Ohm -- LED+ 
+ * L432KC D3 -- 220 Ohm -- LED+ 
  * L432KC GND -- LED-
- * or
- * L432KC D10 -- MOSFET gate
+ * 
+ * L432KC D3 -- OSEPPMTD01 ENA
+ * L432KC D9 -- OSEPPMTD01 In1
+ * L432KC D10 -- OSEPPMTD01 In2
+ * L432KC 5V -- OSEPPMTD01 5V
+ * L432KC GND -- OSEPPMTD01 GND
+ * 
+ * Disconnect the jumper JP1 on the MTD-01 !!!!!
+ * 12V power -- OSEPPMTD01 VCC
+ * 0V power -- OSEPPMTD01 GND
+ *
+ * MOTOR + -- OSEPPMTD01 OUT1
+ * MOTOR - -- OSEPPMTD01 OUT2
  *
  * Operating system OS 6.0
- * Timo Karppinen 4.9.2020  Apache-2.0
+ * Timo Karppinen 3.5.2021  Apache-2.0
  */
 
 #include "mbed.h"
@@ -27,26 +41,44 @@
 // Speed change every x seconds
 #define SpeedChanging_RATE     1s
 
-// Initialise the digital pin D10 as a pwm output
+// Initialise the digital pin D3 as a pwm output
 // The pin needs to be one of the PWM capable pins ! 
-PwmOut pwmMOSFET(D10);
+PwmOut pwmMTD01(D3);
 // Variables for the pwm
 int spCount = 1;
+int spCountStep = 1;
 float speed = 1.0;   // from 0.0 to 1.0 
-float usT = 1000;   // f = 1000 Hz  T = 1 ms = 1000 us
+int usT = 1000;   // f = 1000 Hz  T = 1 ms = 1000 us
                     // f = 10000 Hz  T = 100 us
+                    
+// drive direction for the full bridge driver
+DigitalOut in1(D9);
+DigitalOut in2(D10);
+
+                  
 int main()
 {
-    pwmMOSFET.period_us(usT);
-    pwmMOSFET.write(1.0f);
+    pwmMTD01.period_us(usT);
+    pwmMTD01.write(0.5f);
+    in1.write(false); // forward
+    in2.write(true);
+    ThisThread::sleep_for(3s); 
     while (true) {
         
-        spCount = spCount + 1;
+        spCount = spCount + spCountStep;
         if(spCount == 25){
-            spCount = 1;
+            spCountStep = -1;
             }
-        speed = 1/(float)spCount;
-        pwmMOSFET.write(speed);
+        if(spCount == 1){
+            spCountStep = 1;
+            }
+        speed = 0.04*(float)spCount;
+        pwmMTD01.write(speed);
+        
+        if(spCount == 1){
+            in1.write(!in1.read());
+            in2.write(!in2.read());
+            }
         
         ThisThread::sleep_for(SpeedChanging_RATE);
     }
